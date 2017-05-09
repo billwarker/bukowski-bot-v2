e361bdb2225f4f51de06a9677a2b2bc50c7c54b5
Will Barker <willbarker1995@gmail.com>
2017-05-09 18:48:00 -0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,10 +1,10 @@
 # Bukowski Bot Settings
 
 # Twitter Authentication
-CONSUMER_KEY = '9SNCUL4OKBuldg0pJarUiMifQ'
-CONSUMER_SECRET = 'DFuyhaYLoGNla1AyGn2IyAhJpg89FR3toDDE8OdUkoUn5iV6wX'
-ACCESS_TOKEN_KEY = '819855109064572931-NEthsEB3GcdoKC4XSbMaUDMrHt0h52I'
-ACCESS_TOKEN_SECRET = 'esQF044HGY7G5ZI4YjqeHTgzIddoXYya09K4hkOMvYU3a'
+CONSUMER_KEY = 'Insert your consumer_key here!'
+CONSUMER_SECRET = 'Insert your consumer_secret here!'
+ACCESS_TOKEN_KEY = 'Insert your access_token_key here!'
+ACCESS_TOKEN_SECRET = 'Insert your access_token_secret here!'
 
 # Search parameters
 KEYWORDS = ['Charles Bukowski', 'Bukowski']
